7d8658bf5c1f643ed19531ba8025a55af014e10a
marcinnowo307 <43822211+marcinnowo307@users.noreply.github.com>
2021-05-14 16:48:35 +0200

    Admin Form
    plus some example data source

---
+++ TAB-clinic-GUI/AdminMainForm.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TAB_clinic_Business;
+
+namespace TAB_clinic_GUI
+{
+    public partial class AdminMainForm : Form
+    {
+        public AdminMainForm()
+        {
+            InitializeComponent();
+
+            dataGridView1.DataSource = AdminService.getUsers();
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
