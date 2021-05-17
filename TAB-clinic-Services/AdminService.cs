d4e0220dc893dcd9197e6ab2c575c965f94a92dc
Konfirm44 <tomsitek@gmail.com>
2021-05-17 13:28:43 +0200

    added AdminService class

---
+++ TAB-clinic-Services/AdminService.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using TAB_clinic_Model;
+
+namespace TAB_clinic_Services
+{
+    public class AdminService
+    {
+        public static List<UserModel> UserList()
+        {
+            return UserContext.GetUsers();
+        }
+    }
+}
