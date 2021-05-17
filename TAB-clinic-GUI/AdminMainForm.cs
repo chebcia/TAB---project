b7739cc80c859cb31e56026f8591abf7678f9b88
Konfirm44 <tomsitek@gmail.com>
2021-05-17 15:53:05 +0200

    Moved AddSampleUsers() to a separate SampleDataGenerator class

--- TAB-clinic-GUI/AdminMainForm.cs
+++ TAB-clinic-GUI/AdminMainForm.cs
@@ -10,24 +10,9 @@ namespace TAB_clinic_GUI
         {
             InitializeComponent();
 
-            AddSampleUsers();
+            SampleDataGenerator.AddSampleUsers();
 
             dataGridView1.DataSource = AdminService.UserList();
-        }
-
-        void AddSampleUsers()
-        {
-            try
-            {
-                UserContext.CreateUser("natalia", "cheba", TAB_clinic_Model.ClinicRole.Registrar);
-                UserContext.CreateUser("dr", "augustyn", TAB_clinic_Model.ClinicRole.Doctor);
-                UserContext.CreateUser("jerzy", "bodzenta", TAB_clinic_Model.ClinicRole.LabManager);
-                UserContext.CreateUser("dariusz", "mrozek", TAB_clinic_Model.ClinicRole.LabWorker);
-            }
-            catch (UserAlreadyExistsException)
-            {
-
-            }
         }
     }
 }
