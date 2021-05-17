2a9a1c5efc5dfa709f7a474e085796c44a92b758
Konfirm44 <tomsitek@gmail.com>
2021-05-17 13:42:13 +0200

    sample users

--- TAB-clinic-GUI/AdminMainForm.cs
+++ TAB-clinic-GUI/AdminMainForm.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows.Forms;
+using TAB_clinic_Model;
 using TAB_clinic_Services;
 
 namespace TAB_clinic_GUI
@@ -9,7 +10,24 @@ namespace TAB_clinic_GUI
         {
             InitializeComponent();
 
+            AddSampleUsers();
+
             dataGridView1.DataSource = AdminService.UserList();
+        }
+
+        void AddSampleUsers()
+        {
+            try
+            {
+                UserContext.CreateUser("natalia", "cheba", TAB_clinic_Model.ClinicRole.Registar);
+                UserContext.CreateUser("dr", "augustyn", TAB_clinic_Model.ClinicRole.Doctor);
+                UserContext.CreateUser("jerzy", "bodzenta", TAB_clinic_Model.ClinicRole.LabManager);
+                UserContext.CreateUser("dariusz", "mrozek", TAB_clinic_Model.ClinicRole.LabWorker);
+            }
+            catch (UserAlreadyExistsException)
+            {
+
+            }
         }
     }
 }
