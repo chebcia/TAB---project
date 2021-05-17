620212328b6d7db63562895091171cf0111b9dff
Konfirm44 <tomsitek@gmail.com>
2021-05-17 12:45:00 +0200

    wrapped User class with UserModel

---
+++ TAB-clinic-Model/UserModel.cs
@@ -0,0 +1,52 @@
+﻿using System.Diagnostics;
+using TAB_clinic_Data.Database;
+using static TAB_clinic_Model.ClinicRoleMethods;
+
+namespace TAB_clinic_Model
+{
+    class UserModel
+    {
+        public UserModel(User dbUser)
+        {
+            this.dbUser = dbUser;
+        }
+
+        private readonly User dbUser;
+
+        public string Login
+        {
+            get => dbUser.Login;
+            set => dbUser.Login = value;
+        }
+
+        public string Password
+        {
+            get => dbUser.Password;
+            set => dbUser.Password = value;
+        }
+
+        public ClinicRole Role
+        {
+            get => StringToRole(dbUser.Role);
+            set => dbUser.Role = value.RoleToString();
+        }
+
+        public bool Active
+        {
+            get => dbUser.Active[0] == ('A');
+            set => dbUser.Active = value ? "A" : "N";
+        }
+
+        // TODO: proper validation lol
+        public bool ValidatePassword(string password)
+        {
+            if (password == Password)
+            {
+                return true;
+            }
+
+            Trace.WriteLine("Wrong password");  // should frontend know?
+            return false;
+        }
+    }
+}
