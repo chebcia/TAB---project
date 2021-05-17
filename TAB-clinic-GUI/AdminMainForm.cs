e22ea677febd6373e4f9b3c13984b0bd90edf55d
Konfirm44 <tomsitek@gmail.com>
2021-05-17 13:32:26 +0200

    cleanup

--- TAB-clinic-GUI/AdminMainForm.cs
+++ TAB-clinic-GUI/AdminMainForm.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Windows.Forms;
+﻿using System.Windows.Forms;
 using TAB_clinic_Services;
 
 namespace TAB_clinic_GUI
@@ -11,16 +10,6 @@ namespace TAB_clinic_GUI
             InitializeComponent();
 
             dataGridView1.DataSource = AdminService.UserList();
-        }
-
-        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
-        {
-
-        }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
         }
     }
 }
--- TAB-clinic-Model/UserAlreadyExistsException.cs
+++ TAB-clinic-Model/UserAlreadyExistsException.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TAB_clinic_Model
 {
