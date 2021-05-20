21c7536fea6bdd23f8647642cf39df3218da958b
Łukasz Maroń <43815281+lukaszMaron@users.noreply.github.com>
2021-05-20 17:10:59 +0200

    augustyn changes on lab and admin

--- TAB-clinic-GUI/LabMainForm.Designer.cs
+++ TAB-clinic-GUI/LabMainForm.Designer.cs
@@ -34,6 +34,9 @@ namespace TAB_clinic_GUI
             this.buttonAccept = new System.Windows.Forms.Button();
             this.buttonCancel = new System.Windows.Forms.Button();
             this.buttonNote = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -43,36 +46,35 @@ namespace TAB_clinic_GUI
             this.label1.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(203, 19);
+            this.label1.Size = new System.Drawing.Size(144, 19);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Laboratory Manager Main View";
-            
+            this.label1.Text = "Laboratory Main View";
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 31);
+            this.dataGridView1.Location = new System.Drawing.Point(12, 38);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowTemplate.Height = 25;
-            this.dataGridView1.Size = new System.Drawing.Size(676, 318);
+            this.dataGridView1.Size = new System.Drawing.Size(676, 311);
             this.dataGridView1.TabIndex = 1;
             // 
             // buttonAccept
             // 
-            this.buttonAccept.Location = new System.Drawing.Point(595, 355);
+            this.buttonAccept.Location = new System.Drawing.Point(496, 355);
             this.buttonAccept.Name = "buttonAccept";
             this.buttonAccept.Size = new System.Drawing.Size(93, 33);
             this.buttonAccept.TabIndex = 2;
-            this.buttonAccept.Text = "Accept Exam";
+            this.buttonAccept.Text = "View";
             this.buttonAccept.UseVisualStyleBackColor = true;
             // 
             // buttonCancel
             // 
-            this.buttonCancel.Location = new System.Drawing.Point(496, 355);
+            this.buttonCancel.Location = new System.Drawing.Point(595, 355);
             this.buttonCancel.Name = "buttonCancel";
             this.buttonCancel.Size = new System.Drawing.Size(93, 33);
             this.buttonCancel.TabIndex = 3;
-            this.buttonCancel.Text = "Cancel Exam";
+            this.buttonCancel.Text = "Exit";
             this.buttonCancel.UseVisualStyleBackColor = true;
             // 
             // buttonNote
@@ -81,15 +83,43 @@ namespace TAB_clinic_GUI
             this.buttonNote.Name = "buttonNote";
             this.buttonNote.Size = new System.Drawing.Size(93, 33);
             this.buttonNote.TabIndex = 4;
-            this.buttonNote.Text = "Make Note";
+            this.buttonNote.Text = "Handle";
             this.buttonNote.UseVisualStyleBackColor = true;
             this.buttonNote.Click += new System.EventHandler(this.buttonNote_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(613, 8);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(397, 6);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.ShowCheckBox = true;
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 23);
+            this.dateTimePicker1.TabIndex = 7;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(270, 6);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 23);
+            this.comboBox1.TabIndex = 8;
             // 
             // LabMainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(700, 394);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.buttonNote);
             this.Controls.Add(this.buttonCancel);
             this.Controls.Add(this.buttonAccept);
@@ -110,5 +140,8 @@ namespace TAB_clinic_GUI
         private System.Windows.Forms.Button buttonAccept;
         private System.Windows.Forms.Button buttonCancel;
         private System.Windows.Forms.Button buttonNote;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
