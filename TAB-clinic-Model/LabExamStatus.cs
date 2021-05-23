3ea3cbb304d57edb202004f2092be13dcdc3905c
kmolski <starfighter413@gmail.com>
2021-05-23 14:21:22 +0200

    Add enums for Exam classes

---
+++ TAB-clinic-Model/LabExamStatus.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TAB_clinic_Model
+{
+    public enum LabExamStatus
+    {
+        Requested,
+        CancelledByWorker,
+        CancelledByManager,
+        FinalizedByWorker,
+        FinalizedByManager
+    }
+
+    public static class LabExamStatusMethods
+    {
+        public static string? StatusToStr(this LabExamStatus status)
+        {
+            return status switch
+            {
+                LabExamStatus.Requested => "re",
+                LabExamStatus.CancelledByWorker => "cw",
+                LabExamStatus.CancelledByManager => "cm",
+                LabExamStatus.FinalizedByWorker => "fw",
+                LabExamStatus.FinalizedByManager => "fm",
+                _ => throw new ArgumentOutOfRangeException()
+            };
+        }
+
+        public static LabExamStatus StrToStatus(string statusStr)
+        {
+            return statusStr switch
+            {
+                "re" => LabExamStatus.Requested,
+                "cw" => LabExamStatus.CancelledByWorker,
+                "cm" => LabExamStatus.CancelledByManager,
+                "fw" => LabExamStatus.FinalizedByWorker,
+                "fm" => LabExamStatus.FinalizedByManager,
+                _ => throw new ArgumentOutOfRangeException()
+            };
+        }
+
+    }
+}
