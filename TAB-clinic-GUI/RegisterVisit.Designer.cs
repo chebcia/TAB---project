94a7c128500d83859a9b9f94ac8446dc342c75e5
Natalia Cheba <naticheb@onet.eu>
2021-06-20 16:13:53 +0200

    Fixed scheduling visits in the past

--- TAB-clinic-GUI/RegisterVisit.Designer.cs
+++ TAB-clinic-GUI/RegisterVisit.Designer.cs
@@ -38,10 +38,10 @@ namespace TAB_clinic_GUI
             // 
             // buttonSave
             // 
-            this.buttonSave.Location = new System.Drawing.Point(305, 54);
-            this.buttonSave.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.buttonSave.Location = new System.Drawing.Point(566, 115);
+            this.buttonSave.Margin = new System.Windows.Forms.Padding(6, 4, 6, 4);
             this.buttonSave.Name = "buttonSave";
-            this.buttonSave.Size = new System.Drawing.Size(82, 22);
+            this.buttonSave.Size = new System.Drawing.Size(152, 47);
             this.buttonSave.TabIndex = 0;
             this.buttonSave.Text = "Save";
             this.buttonSave.UseVisualStyleBackColor = true;
@@ -50,9 +50,10 @@ namespace TAB_clinic_GUI
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(8, 9);
+            this.label4.Location = new System.Drawing.Point(15, 19);
+            this.label4.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(60, 15);
+            this.label4.Size = new System.Drawing.Size(123, 32);
             this.label4.TabIndex = 7;
             this.label4.Text = "Date visit*";
             this.label4.Click += new System.EventHandler(this.label4_Click);
@@ -60,18 +61,19 @@ namespace TAB_clinic_GUI
             // inputDoctor
             // 
             this.inputDoctor.FormattingEnabled = true;
-            this.inputDoctor.Location = new System.Drawing.Point(116, 30);
-            this.inputDoctor.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.inputDoctor.Location = new System.Drawing.Point(215, 64);
+            this.inputDoctor.Margin = new System.Windows.Forms.Padding(6, 4, 6, 4);
             this.inputDoctor.Name = "inputDoctor";
-            this.inputDoctor.Size = new System.Drawing.Size(273, 23);
+            this.inputDoctor.Size = new System.Drawing.Size(504, 40);
             this.inputDoctor.TabIndex = 11;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(8, 32);
+            this.label6.Location = new System.Drawing.Point(15, 68);
+            this.label6.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(81, 15);
+            this.label6.Size = new System.Drawing.Size(164, 32);
             this.label6.TabIndex = 12;
             this.label6.Text = "Select doctor*";
             this.label6.Click += new System.EventHandler(this.label6_Click);
@@ -80,23 +82,25 @@ namespace TAB_clinic_GUI
             // 
             this.inputDate.CustomFormat = "  MM/dd/yyyy  h:mm tt";
             this.inputDate.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-            this.inputDate.Location = new System.Drawing.Point(116, 7);
+            this.inputDate.Location = new System.Drawing.Point(215, 15);
+            this.inputDate.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.inputDate.Name = "inputDate";
-            this.inputDate.Size = new System.Drawing.Size(273, 23);
+            this.inputDate.Size = new System.Drawing.Size(504, 39);
             this.inputDate.TabIndex = 14;
+            this.inputDate.ValueChanged += new System.EventHandler(this.inputDate_ValueChanged);
             // 
             // RegisterVisitForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(395, 82);
+            this.ClientSize = new System.Drawing.Size(734, 175);
             this.Controls.Add(this.inputDate);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.inputDoctor);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.buttonSave);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Margin = new System.Windows.Forms.Padding(6, 4, 6, 4);
             this.Name = "RegisterVisitForm";
             this.Text = "Make an appointment";
             this.Load += new System.EventHandler(this.Form3_Load);
